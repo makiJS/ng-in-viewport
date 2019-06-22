26f774f6bb3961054507d8477c3427481c3b9c33
Piotr Stępniewski <k3nsei.pl@gmail.com>
2019-06-22 22:04:24 +0200

    chore: Setup new workspace
    Signed-off-by: Piotr Stępniewski <k3nsei.pl@gmail.com>

--- libs/ng-in-viewport/src/lib/ng-in-viewport.module.spec.ts
+++ libs/ng-in-viewport/src/lib/ng-in-viewport.module.spec.ts
@@ -1,7 +1,7 @@
 import { async, TestBed } from '@angular/core/testing';
 import { InViewportModule } from './ng-in-viewport.module';
 
-describe('NgInViewportModule', () => {
+describe('InViewportModule', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [InViewportModule]
